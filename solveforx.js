7c37fe5e3648561520680ab6d943e6ad5cd6328d
marekdelorme <144932928+marekdelorme@users.noreply.github.com>
2023-10-30 11:33:16 -0600

    Add files via upload

---
+++ solveforx.js
@@ -0,0 +1,8 @@
+// Define the equation
+const x = (2 * Math.pow(12, 2)) / (6 + 7);
+
+// Print the result to the console
+console.log("Result:", x);
+
+// Print the type of the variable
+console.log("Type of x:", typeof x);

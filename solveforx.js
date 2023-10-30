9b0776fa91e9e5404bf9f05a639bc0a82dab7660
marekdelorme <144932928+marekdelorme@users.noreply.github.com>
2023-10-30 11:45:26 -0600

    Update solveforx.js

--- solveforx.js
+++ solveforx.js
@@ -2,7 +2,7 @@
 const x = (2 * Math.pow(12, 2)) / (6 + 7);
 
 // Print the result to the console
-console.log("Result:", x);
+console.log("Result:" + x);
 
 // Print the type of the variable
-console.log("Type of x:", typeof x);
+console.log("Type of x:" + typeof x);

f6fa90209cd8c455ba144fa8d84eaa13e575196f
marekdelorme <144932928+marekdelorme@users.noreply.github.com>
2023-10-30 11:47:39 -0600

    Update solveforx.js

--- solveforx.js
+++ solveforx.js
@@ -1,8 +1,12 @@
-// Define the equation
-const x = (2 * Math.pow(12, 2)) / (6 + 7);
+document.addEventListener("DOMContentLoaded", function () {
+  // This code will run after the HTML content has been fully loaded
 
-// Print the result to the console
-console.log("Result:" + x);
+  // Define the equation
+  const x = (2 * Math.pow(12, 2)) / (6 + 7);
 
-// Print the type of the variable
-console.log("Type of x:" + typeof x);
+  // Print the result to the console
+  console.log("Result:", x);
+
+  // Print the type of the variable
+  console.log("Type of x:", typeof x);
+});
